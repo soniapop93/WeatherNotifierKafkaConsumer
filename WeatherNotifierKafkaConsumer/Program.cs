d56227a894f6b28cb534a4cf8ec34c4ddda8b3ef
soniapop93 <popsonia85@gmail.com>
2023-06-22 20:15:27 +0300

    first commit

---
+++ WeatherNotifierKafkaConsumer/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        /*
+           =============================================================
+           =============================================================
+              The API endoints used in this script are free to use.
+              https://ipinfo.io/ip
+              https://api.techniknews.net/ipgeo/ + ipAddress
+              https://api.open-meteo.com/
+
+            Kafka Producer:
+                - consumes data from Producer WeatherNotifierKafkaProducer
+                - sends a message that the weather changed
+
+           =============================================================
+           =============================================================
+        */
+
+    }
+}
